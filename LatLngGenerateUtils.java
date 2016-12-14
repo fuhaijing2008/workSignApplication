e32f47186f2b427d20eccda0b6bd921f3f97ad98
fuhaijing2008 <fuhaijing2008@gmail.com>
2016-12-14 10:24:03 +0800

    消除警告

---
+++ LatLngGenerateUtils.java
@@ -0,0 +1,28 @@
+package com.example.esc.worksigninapplication;
+
+/**
+ * Created by esc on 2016/12/13  .
+ */
+public class LatLngGenerateUtils {
+
+    public static String generateRandomLat(){
+
+        String lat = "39.332";
+        return lat+ generateRandomElevenDates();
+    }
+    public static String generateRandomLng(){
+        String lng = "116.977";
+        return lng+ generateRandomElevenDates();
+    }
+    public static String generateRandomElevenDates(){
+
+        String num = "";
+        for(int i=0;i<11;i++)
+            num = num+ generateRandomNum();
+        return num;
+    }
+    public static String generateRandomNum(){
+        int random=(int)(Math.random()*10);
+        return random+"";
+    }
+}
